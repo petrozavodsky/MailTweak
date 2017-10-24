8923f422f4d05ff518b6a8842fdcca08dea3cd5c
petrozvodsky <petrozavodsky@gmail.com>
2017-10-24 11:00:33 +0300

    Add options helper

---
+++ src/MailTweak/Classes/Options.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace MailTweak\Classes;
+
+
+class Options {
+
+	private $options = [];
+
+	public function __construct( $options, $slug ) {
+		$this->options = $options;
+	}
+
+	public function update($array= []) {
+
+	}
+
+
+	public function flush() {
+
+	}
+
+}
