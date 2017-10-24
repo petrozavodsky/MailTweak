32b111d1c199df4e754500851e81e09866a9d4cd
petrozavodsky <petrozavodsky@gmail.com>
2017-10-24 20:45:07 +0300

    Fix sender

--- src/MailTweak/Classes/MailProxy.php
+++ src/MailTweak/Classes/MailProxy.php
@@ -13,27 +13,40 @@ class MailProxy {
 		'SMTPAuth'   => true,
 		'Username'   => '',
 		'Password'   => '',
-		'Sender'     => '',
 		'From'       => '',
 		'FromName'   => '',
-		'AddReplyTo' => ''
 	];
 
 	public function __construct() {
-		add_action( 'phpmailer_init', 'proxy' );
+		add_action( 'phpmailer_init', [ $this, 'proxy' ] );
 	}
 
-	public function proxy( $mailer ) {
-		$mailer->Mailer     = self::$options['Mailer'];
-		$mailer->Port       = self::$options['Port'];
-		$mailer->host       = self::$options['host'];
-		$mailer->SMTPSecure = self::$options['SMTPSecure'];
-		$mailer->SMTPAuth   = self::$options['SMTPAuth'];
-		$mailer->Username   = self::$options['Username'];
-		$mailer->Password   = self::$options['Password'];
-		$mailer->Sender     = self::$options['Sender'];
-		$mailer->From       = self::$options['From'];
-		$mailer->AddReplyTo = self::$options['AddReplyTo'];
+	public function proxy( ) {
+		global $mailer;
+
+		$options = shortcode_atts(
+			self::$options,
+			Options::get()
+		);
+
+		if ( is_email( $options['From'] ) && '' !== $options['host'] ) {
+
+			$mailer->Mailer     = $options['Mailer'];
+			$mailer->Port       = $options['Port'];
+			$mailer->host       = $options['host'];
+			$mailer->SMTPSecure = $options['SMTPSecure'];
+			$mailer->SMTPAuth   = ( Options::get( 'SMTPAuth' ) === 'yes' ? true : false );
+
+			if ( Options::get( 'SMTPAuth' ) === 'yes' ) {
+				$mailer->Username = $options['Username'];
+				$mailer->Password = $options['Password'];
+			}
+
+			$mailer->Sender   = $options['From'];
+			$mailer->From     = $options['From'];
+			$mailer->FromName = $options['FromName'];
+			$mailer->AddReplyTo( $options['From'], $options['FromName'] );
+		}
 
 		return $mailer;
 	}
