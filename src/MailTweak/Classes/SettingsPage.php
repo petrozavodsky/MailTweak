13d9dc84973f48c687e600738d5a4f71622d342a
petrozvodsky <petrozavodsky@gmail.com>
2017-10-24 11:08:57 +0300

    add_settings_page

--- src/MailTweak/Classes/SettingsPage.php
+++ src/MailTweak/Classes/SettingsPage.php
@@ -20,7 +20,32 @@ class SettingsPage {
 		$this->settings_url = $this->slug . '-settings';
 		$this->option_base  = $this->slug;
 
+		add_action( 'admin_menu', [ $this, 'add_settings_page' ] );
+
 	}
 
 
+	public function add_settings_page() {
+		$page = add_options_page(
+			__( 'SMTP SETTINGS', $this->textdomine ),
+			__( 'SMTP SETTINGS', $this->textdomine ),
+			'activate_plugins',
+			$this->settings_url,
+			function () {
+				$url   = admin_url( 'options.php' );
+				$title = get_admin_page_title();
+				echo "<div class='wrap'>";
+				echo "<h2>{$title}</h2>";
+				echo "<form method='POST' action='{$url}'>";
+				do_action( 'MailTweak__settings_form_before' );
+				settings_fields( $this->option_base );
+				do_settings_sections( $this->settings_url );
+				do_action( 'MailTweak__settings_form_after' );
+				submit_button();
+				echo "</form>";
+				echo "</div>";
+			}
+		);
+
+	}
 }
