bbdc0eb0d96c52eaded2cce1c26542b5750662e2
petrozvodsky <petrozavodsky@gmail.com>
2017-10-26 17:48:32 +0300

    change Mail replace text

--- src/MailTweak/Classes/GetTextParser.php
+++ src/MailTweak/Classes/GetTextParser.php
@@ -16,23 +16,25 @@ class GetTextParser {
 		add_filter( 'gettext', [ $this, "get_data" ], 10, 2 );
 
 		$this->patterns = [
-			'[%s] New User Registration'              => [ 'new_user_register', __( 'New user create', MailTweak::$textdomine ) ],
-			'[%s] Password Reset'                     => [ 'reset_password', __( 'Resset password', MailTweak::$textdomine ) ],
+			'[%s] Your username and password info'    => [ 'new_user_register', __( 'New user create (admin alert)', MailTweak::$textdomine ) ],
+			'[%s] New User Registration'              => [ 'create_new_user', __( 'New user register (admin alert)', MailTweak::$textdomine ) ],
+			'Password changed for user: %s'           => [ 'password_changed_alert', __( 'User password change (admin alert)', MailTweak::$textdomine ) ],
+			'[%s] Password Reset'                     => [ 'reset_password', __( 'Resset password (user alert)', MailTweak::$textdomine ) ],
 			'[%s] Notice of Password Change'          => [ 'change_password_alert', __( 'New password created (user alert)', MailTweak::$textdomine ) ],
-			'[%s] Your username and password info'    => [ 'create_new_user', __( 'New password register (admin alert)', MailTweak::$textdomine ) ],
-			'[%1$s] Please moderate: \"%2$s\"'        => [ 'comment_added', __( 'Comment added', MailTweak::$textdomine ) ],
-			'Hi %1$s, comment %2$s has been approved' => [ 'comment_approved', __( 'Comment Approved', MailTweak::$textdomine ) ]
+			'[%1$s] Please moderate: "%2$s"'          => [ 'comment_added', __( 'Comment added (admin alert)', MailTweak::$textdomine ) ],
+			'Hi %1$s, comment %2$s has been approved' => [ 'comment_approved', __( 'Comment Approved (user alert)', MailTweak::$textdomine ) ]
 		];
 
 	}
 
-	public function get_data( $translation, $text) {
+	public function get_data( $translation, $text ) {
 		$patterns = $this->patterns;
 
 		if ( array_key_exists( $text, $patterns ) ) {
 
 			add_filter( 'wp_mail', function ( $array ) use ( $patterns, $text ) {
-				$array = apply_filters( 'MailTweak__message_send', $array , $patterns[ $text ][0], $text );
+				$array = apply_filters( 'MailTweak__message_send', $array, $patterns[ $text ][0], $text );
+
 				return $array;
 			} );
 		}
--- src/MailTweak/Classes/MessageMailShortcodes.php
+++ src/MailTweak/Classes/MessageMailShortcodes.php
@@ -6,14 +6,17 @@ namespace MailTweak\Classes;
 class MessageMailShortcodes {
 
 	private $shortcode_name = 'mt_variable';
-	private $vars = [];
+	private $type;
 
 	public function __construct() {
+
+		add_filter( 'MailTweak__message_mapper_fields_extractor_filter', [ $this, 'filter' ], 10, 4 );
 		add_shortcode( $this->shortcode_name, [ $this, 'shortcode' ] );
-		add_action( 'MailTweak__message_mapper_fields_extractor_filter', [ $this, 'filter' ], 10, 4 );
+
 	}
 
 	public function shortcode( $attrs ) {
+		$GLOBALS['MailTweak_MapperHelper_tmp'];
 		$attrs = shortcode_atts(
 			[
 				'type' => false,
@@ -21,9 +24,13 @@ class MessageMailShortcodes {
 			$attrs
 		);
 
+		$vars = $GLOBALS['MailTweak_MapperHelper_tmp'][ $this->type ];
+
+
+
 		if ( false !== $attrs['type'] ) {
-			if ( array_key_exists( $attrs['type'], $this->vars ) ) {
-				return $this->vars[ $attrs['type'] ];
+			if ( array_key_exists( $attrs['type'], $vars ) ) {
+				return $vars[ $attrs['type'] ];
 			}
 		}
 
@@ -31,8 +38,9 @@ class MessageMailShortcodes {
 
 	}
 
-	public function filter( $value, $type, $field, $vars ) {
-		$this->vars = $vars[$type];
+	public function filter( $value, $type, $field ) {
+
+		$this->type = $type;
 
 		if ( in_array( $field, [ 'message', 'subject' ] ) ) {
 			return do_shortcode( $value );
