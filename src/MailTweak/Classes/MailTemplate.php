f73e1a186c474bb46d63acb6bb87c0c4c8cb78d6
petrozavodsky <petrozavodsky@gmail.com>
2017-11-04 13:28:43 +0300

    themplate logic

--- src/MailTweak/Classes/MailTemplate.php
+++ src/MailTweak/Classes/MailTemplate.php
@@ -19,7 +19,13 @@ class MailTemplate {
 
 	public function message_filter( $args ) {
 
-		$args['message'] = $this->option['header-message'] . $args['message'] . $this->option['footer-message'];
+		if ( ! empty( $this->option['header-message'] ) ) {
+			$args['message'] = $this->option['header-message'] . "\r\n" . $args['message'];
+		}
+
+		if ( ! empty( $this->option['footer-message'] ) ) {
+			$args['message'] = $args['message'] . "\r\n" . $this->option['footer-message'];
+		}
 
 		return $args;
 	}
